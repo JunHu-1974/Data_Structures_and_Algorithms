fc5f42fa1e650fbf793923331021127c5d8bd18e
JunHu-1974 <hujun918@yahoo.com>
2025-08-18 17:12:06 +0000

    Basic BFS and DFS algorithms

---
+++ python/binary_tree_traversal/binary_tree.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+class TreeNode(object):
+    def __init__(self, val: int = 0, left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self) -> str:
+        return 'val: {}, left: ({}), right: ({})'.format(self.val, self.left, self.right)
+
+def listToTreeNode(values: list = []) -> TreeNode:
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = [root]
+    parent = 0
+    curr = 1
+    while curr < len(values):
+        node = queue[parent]
+        parent += 1
+        value = values[curr]
+        curr += 1
+        if value:
+            node.left = TreeNode(value)
+            queue.append(node.left)
+
+        if curr < len(values):
+            value = values[curr]
+            curr += 1
+            if value:
+                node.right = TreeNode(value)
+                queue.append(node.right)
+    return root
+
+def main() -> None:
+    tree = listToTreeNode([6, 2, 7, 1, 4, None, 9, None, None, 3, 5, 8])
+    print(tree)
+
+if __name__ == '__main__':
+    main()
---
+++ python/binary_tree_traversal/dfs_recursion.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+from binary_tree import TreeNode, listToTreeNode
+
+def visit(node: TreeNode) -> Optional[int]:
+    if node:
+        return node.val
+    else:
+        return None
+    
+def depth_first_search(root: TreeNode, order: str) -> list:
+    output = []
+    if not root:
+        return output
+    left_tree = depth_first_search(root.left, order)
+    right_tree = depth_first_search(root.right, order)
+    if order == 'pre':
+        output = [visit(root)] + left_tree + right_tree
+    elif order == 'in':
+        output = left_tree + [visit(root)] + right_tree
+    else:
+        output = left_tree + right_tree + [visit(root)]
+    return output
+
+def main() -> None:
+    tree = listToTreeNode([6, 2, 7, 1, 4, None, 9, None, None, 3, 5, 8])
+    print(tree)
+    output= depth_first_search(tree, 'pre')
+    print(output)
+    output= depth_first_search(tree, 'in')
+    print(output)
+    output= depth_first_search(tree, 'post')
+    print(output)
+
+if __name__ == '__main__':
+    main()
