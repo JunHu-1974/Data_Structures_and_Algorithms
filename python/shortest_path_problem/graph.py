4737355648fb1b10bec45ed2c15939fa0dfe60da
JunHu-1974 <hujun918@yahoo.com>
2025-08-30 19:12:39 +0000

    Better implementation for Graph class

--- python/shortest_path_problem/graph.py
+++ python/shortest_path_problem/graph.py
@@ -1,18 +1,44 @@
+from typing import Optional
+
 class Graph(object):
     def __init__(self, size: int = 10):
         self.size = size
-        self.adj_matrix = [size * [float('inf')] for i in range(size)]
         self.vertices = size * [None]
+        self.neighbors = size * [None]
+        self.adj_matrix = [size * [float('inf')] for i in range(size)]
     
-    def add_vertex(self, index: int, data: tuple[int, int]) -> None:
-        if index >= 0 and index < self.size:
+    def add_vertex(self, index: int, data: Optional[tuple[int, int]]) -> None:
+        if index < 0 or index >= self.size:
+            return
+        if data is None:
+            if self.neighbors:
+                return
+            else:
+                self.vertices[index] = None
+                self.neighbors[index] = None
+        else:
             self.vertices[index] = data
+            if self.neighbors[index] is None:
+                self.neighbors[index] = set()
 
     def add_edge(self, start: int, end: int, weight: int, directed: bool) -> None:
-        if start >= 0 and start < self.size and end >= 0 and end < self.size:
-            self.adj_matrix[start][end] = weight
+        if start < 0 or start >= self.size or self.vertices[start] is None:
+            return
+        if end < 0 or end >= self.size or self.vertices[end] is None:
+            return
+        if weight == float('inf'):
+            if end in self.neighbors[start]:
+                self.neighbors[start].remove(end)
             if not directed:
-                self.adj_matrix[end][start] = weight
+                if start in self.neighbors[end]:
+                    self.neighbors[end].remove(start)
+        else:
+            self.neighbors[start].add(end)
+            if not directed:
+                self.neighbors[end].add(start)
+        self.adj_matrix[start][end] = weight
+        if not directed:
+            self.adj_matrix[end][start] = weight
 
 def main() -> None:
     graph = Graph(7)
@@ -36,7 +62,7 @@ def main() -> None:
 
     graph.add_edge(3, 1, 1, True)
     for i in range(graph.size):
-        print(graph.vertices[i], graph.adj_matrix[i])
+        print(graph.vertices[i], graph.adj_matrix[i], graph.neighbors[i])
 
 if __name__ == '__main__':
     main()
